86fa25de68264b569af3c86c086b007b30927083
ieqlzhang <ieqlzhang@gmail.com>
2025-10-15 13:20:47 +0000

    fixed the problem where app.tsx does not use react but it was imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { generateClient } from 'aws-amplify/data';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import type { Schema } from '../amplify/data/resource';
